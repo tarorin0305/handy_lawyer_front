96a763014865ceb333ed03a54f6a20f95570c730
tarorin0305 <dayutakatarorin@gmail.com>
2021-01-15 23:31:14 +0900

    Integrated Tailwind CSS into this project cf: https://tailwindcss.nuxtjs.org/

---
+++ pages/corporate_law.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-5">
+    <NuxtLink to="/">to home</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <NuxtLink to="/corporate_law">Corporate Law</NuxtLink>
+</template>
