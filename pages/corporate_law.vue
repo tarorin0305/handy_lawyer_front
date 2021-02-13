a6d4387e1023ec6f1f36e5984e2482cfab53c7e7
tarorin0305 <dayutakatarorin@gmail.com>
2021-02-14 00:14:33 +0900

    フォームから条文番号を指定して条文内容をリクエストできるようにした

--- pages/corporate_law.vue
+++ pages/corporate_law.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="p-5">
     <NuxtLink to="/" class="m-2 p-2 border-2 border-red-500">to home</NuxtLink>
-    <div class="mt-5 ml-2">
-      <label for="hoge">会社法条文</label>
-      <input type="text" name="article" placeholder="123" class="ml-2 border-2">
-      <input type="submit" v-on:click="click">
+    <div class="mt-5 ml-2"> 
+      <form @submit.prevent id="main_form" action="#">
+        <label for="hoge">会社法条文</label>
+        <input type="text" name="article" placeholder="123" class="ml-2 border-2" id="main_text">
+        <input type="submit" v-on:click="click">
+      </form>
     </div>
     <div>
       <!-- {{ data.content }} -->
@@ -24,7 +26,7 @@
     // },
     methods: {
       click() {
-        const url = "api/v0/corporate_law?article=123";
+        const url = `api/v0/corporate_law?article=${document.forms.main_form.main_text.value}`;
         const response = this.$axios.$get(url)
           .then( response => {
             console.log(response);
